6c06709bac07157bc4d58db46f6e95155dfb6500
keisuke.momohira <keisuke.momohira@axas-japan.co.jp>
2021-06-08 04:07:32 +0900

    Add validation

--- src/tasklist/UpdateServlet.java
+++ src/tasklist/UpdateServlet.java
@@ -2,8 +2,10 @@ package tasklist;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.tasks;
+import models.validators.TasksValidator;
 import utils.DBUtil;
-
 /**
  * Servlet implementation class UpdateServlet
  */
@@ -49,6 +51,19 @@ public class UpdateServlet extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             m.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
+            // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+            List<String> errors = TasksValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
+
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("tasks", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+                rd.forward(request, response);
+            } else {
             // データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
@@ -60,6 +75,7 @@ public class UpdateServlet extends HttpServlet {
 
             // indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 }
